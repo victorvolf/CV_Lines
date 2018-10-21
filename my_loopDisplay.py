70c74034e23906840dcd1ca8bca5c0d01fdb3f6d
Victor Volf <csufsn@antares.astro.warwick.ac.uk>
2018-10-21 19:23:27 +0100

    Sal

--- my_loopDisplay.py
+++ my_loopDisplay.py
@@ -7,8 +7,12 @@ Plots SDSS DR12 spectra
 
 import os, sys, argparse
 import numpy as np
+
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
 import astropy.io.fits as fits
+from astropy.table import Table, Column
 from trm import subs
 
 parser = argparse.ArgumentParser(description = usage)
@@ -29,68 +33,75 @@ parser.add_argument('-n',
 args = parser.parse_args()
 sdss = args.sdss
 
+avg_flux = 0
+t = Table(names=('H-alpha', 'H-alpha σ^2', 'H-beta', 'H-beta σ^2', 'HeII', 'HeII σ^2'), 
+		  dtype=('f8', 'f8', 'f8', 'f8', 'f8', 'f8'))
+
+def plot_spectra(w, f):
+    nb,d = np.histogram(w,args.nbin)
+    wb,d = np.histogram(w,args.nbin, weights=w)
+    fb,d = np.histogram(w,args.nbin, weights=f)
+    ok = nb > 0
+    wb, fb = wb[ok] / nb[ok], fb[ok] / nb[ok] 
+    fhi = 1.1 * np.percentile(fb, 99.9)
+	
+    fig=plt.figure(figsize = (12, 6))
+    plt.plot(wb, fb, 'b')
+    title = 'H-alpha lines of ' + spec[spec.rfind('/') + 1:] + ', ' + pos
+    plt.title(title.replace('_', '-'))
+    plt.xlabel('Wavelength [\AA]')
+    plt.show()
+    
+def total_flux(wavelength):
+    _index = np.nonzero(w > wavelength)[0][0]
+    _range = range(_index - 23, _index + 23)
+    
+    t_flux = sum([(w[i] - w[i-1]) * f[i] for i in _range])
+    t_var = sum([(1/iv[i]) * (w_d[i] ** 2) for i in _range if iv[i] != 0])
+    return t_flux, t_var
+    
 for spec in sdss:
+    #print(spec)
     hdul = fits.open(spec)
     head  = hdul[0].header
     ra  = head['PLUG_RA']/15.
     dec = head['PLUG_DEC']
     pos = subs.d2hms(ra, sep = ' ', dp = 2) + ' ' + \
 		  subs.d2hms(dec, sep = ' ', dp = 1, sign = True)
-    print('Position = ',pos)
-	
-	# Store the range of flux and wavelength in arrays
-    #table_bLines = hdul[3].data  
+    #print('Position = ',pos)
+
+    #print(hdul[3].data)
+   
+   	# Store fluxes and wavelengths in arrays 
     table = hdul[1].data
     w  = subs.vac2air(10. ** table['loglam'])
     f  = table['flux']
     iv = table['ivar']
+    w_d = table['wdisp']
     ok = iv > 0.
     w  = w[ok]
     f  = f[ok]
+    w_d = w_d[ok]
     
-    # Find the H-apha in the array of wavelengths
-    for i in range(w.size // 2, w.size - 1):
-        if w[i] > 6562.66:
-            h_alpha_pos = i
-            break
-            
-    # Calculate the total flux of H-alpha
-    total_flux = 0
-    for i in range(h_alpha_pos - 23, h_alpha_pos + 23):
-        total_flux += (w[i] - w[i - 1]) * f[i]
-    print('Total flux of H-alpha = ', total_flux)
+    #for i in range(0, w.size - 1):
+    #   print(w[i] * 1.0)
+
+    t.add_row([total_flux(6561.14892578)[0], total_flux(6561.14892578)[1], \
+			   total_flux(4862.712890625)[0], total_flux(4862.712890625)[1], \
+			   total_flux(4686.82128906)[0], total_flux(4686.82128906)[1]])
+			   
+    '''#Plot the entire spectra
+    plot_spectra(w,f)
     
-	# Plot the entire spectra
-    nb,d = np.histogram(w,args.nbin)
-    wb,d = np.histogram(w,args.nbin, weights = w)
-    fb,d = np.histogram(w,args.nbin, weights = f)
-    ok = nb > 0
-    wb = wb[ok] / nb[ok]
-    fb = fb[ok] / nb[ok]
-    fhi = 1.1 * np.percentile(fb, 99.9)
-
-    fig=plt.figure(figsize = (12, 6))
-    plt.plot(wb, fb, 'b')
-    title = spec[spec.rfind('/') + 1:] + ', ' + pos
-    plt.title(title.replace('_', '-'))
-    plt.xlabel('Wavelength [\AA]')
-    plt.show()
+    # Plot H-alpha lines
+    plot_spectra(w[ha_index - 23:ha_index + 23], \
+                 f[ha_index - 23:ha_index + 23])
     
-    # Plot of H-alpha lines
-    w_p = w[h_alpha_pos - 23:h_alpha_pos + 23]
-    f_p = f[h_alpha_pos - 23:h_alpha_pos + 23]
-    nb,d = np.histogram(w_p,args.nbin)
-    wb,d = np.histogram(w_p,args.nbin, weights=w_p)
-    fb,d = np.histogram(w_p,args.nbin, weights=f_p)
-    ok = nb > 0
-    wb = wb[ok] / nb[ok]
-    fb = fb[ok] / nb[ok]
-    fhi = 1.1 * np.percentile(fb, 99.9)
-
-    fig=plt.figure(figsize = (12, 6))
-    plt.plot(wb, fb, 'b')
-    title = spec[spec.rfind('/') + 1:] + ', ' + pos
-    plt.title(title.replace('_', '-'))
-    plt.xlabel('Wavelength [\AA]')
-    plt.show()
-
+    # Plot H-beta lines
+    plot_spectra(w[hb_index - 25:hb_index + 25], \
+                 f[hb_index - 25:hb_index + 25])
+                 
+    # Plot HeII lines
+    plot_spectra(w[he2_index - 25:he2_index + 25], \
+                 f[he2_index - 25:he2_index + 25])'''
+print(t)
