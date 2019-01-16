55a8d328345bec2c4e06d847227d2c597e3f1204
Victor Volf <csufsn@antares.astro.warwick.ac.uk>
2019-01-16 19:46:52 +0000

    coaie

--- RandomForrestClassifirer.py
+++ RandomForrestClassifirer.py
@@ -10,11 +10,14 @@ import astropy.io.fits as fits
 from astropy.table import Table, Column
 from trm import subs
 from itertools import zip_longest as longest
+from collections import defaultdict
 
+from PRF import prf
 from sklearn import ensemble as ens
 from sklearn import model_selection as cv
 from sklearn.metrics import roc_curve, auc
 from sklearn.utils import shuffle
+from sklearn.preprocessing import StandardScaler
 
 def perf_measure(y_actual, y_hat):
     TP, FP, TN, FN = 0, 0, 0, 0
@@ -39,38 +42,41 @@ df = shuffle(df)
 classT = df['class'].to_frame()
 df = df.drop(['Name'], axis=1)
 df = df.drop(['class'], axis=1)
+df = df.drop(['HeI 1'], axis=1)
+df = df.drop(['HeII Right'], axis=1)
 
 kf = cv.KFold(10)
 avg_acc = 0
-imp = [0] * 30
+importances = defaultdict(float)
 performance=[0, 0, 0, 0]
 
 for train_index, test_index in kf.split(df):
     xTrain, xTest = df.iloc[train_index, :], df.iloc[test_index, :]
     yTrain, yTest = classT.iloc[train_index, :], classT.iloc[test_index, :]
     
-    rfc = ens.RandomForestClassifier(n_estimators = 100,
+    
+    rfc = prf(n_estimators=10, bootstrap=True, keep_proba=0.05)
+    '''rfc = ens.RandomForestClassifier(n_estimators = 500,
                                      n_jobs = -1, 
                                      criterion = 'entropy',
                                      max_features = None,
                                      class_weight = 'balanced_subsample',
-                                     min_impurity_decrease = 0.0001,
-                                     oob_score = True)
+                                     oob_score = True,
+                                     random_state = 5)'''
     
-    rfc.fit(xTrain.values, yTrain.values.ravel())
+    rfc.fit(X = xTrain.values, y = yTrain.values.ravel())
     predictions = rfc.predict(xTest)
     fpr,tpr,threshold = roc_curve(yTest['class'], predictions, pos_label = 1)
     
-    p= perf_measure(list(yTest['class']), predictions)
-    
+    p = perf_measure(list(yTest['class']), predictions)
     for i in range(0, 4):
         performance[i] += p[i]
     
-    imp += rfc.feature_importances_
+    for feature, importance in zip(df.columns, rfc.feature_importances_):
+        importances[feature] += importance
     avg_acc += auc(fpr, tpr)
-avg_acc /= 10
+print(' TP   FP  TN    FN\n', performance)
+print(avg_acc / 10)
+for w in sorted(importances, key=importances.get, reverse=True):
+    print("%s:" % w, importances[w])
 
-print(' TP   FP  TN    FN')
-print(performance)
-print(avg_acc)
-print(imp)
--- my_loopDisplay.py
+++ my_loopDisplay.py
@@ -8,6 +8,7 @@ import time
 import os, sys, argparse
 import numpy as np
 import pandas as pd
+import random
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
@@ -16,10 +17,6 @@ import astropy.io.fits as fits
 from astropy.table import Table, Column
 from trm import subs
 from itertools import zip_longest as longest
-
-from sklearn import ensemble as ens
-from sklearn import model_selection as cv
-from sklearn.metrics import roc_curve, auc
 
 parser = argparse.ArgumentParser(description = usage)
 
@@ -38,7 +35,7 @@ parser.add_argument('-n',
 # OK, done with arguments.
 args = parser.parse_args()
 sdss = args.sdss
-timp = time.time()
+start = time.time()
 
 t = Table(names=('Name',
 				 'H_alpha Left', 'H_alpha 1', 'H_alpha 2', 'H_alpha Right', 'H_alpha v',
@@ -46,11 +43,14 @@ t = Table(names=('Name',
 				 'HeI Left',     'HeI 1',     'HeI 2',     'HeI Right',     'HeI v',
 				 'HeII Left',    'HeII 1',    'HeII 2',    'HeII Right',    'HeII v', 
 				 'ArIII Left',   'ArIII 1',   'ArIII 2',   'ArIII Right',   'ArIII v', 
-				 'x3 Left',     'x3 1',     'x3 2',     'x3 Right',     'x3 v', 
-				 'class'), 
-		  dtype=('U15','f4','f4','f4','f4','f4','f4','f4','f4',
-		         'f4','f4','f4','f4','f4','f4','f4','f4','f4',
-		         'f4','f4','f4','f4','f4','f4','f4','f4','f4','f4','f4','f4','f4','i2'))
+				 'I1 - G1','G1 - R1','class'), 
+		  dtype=('U15',
+                 'f4',
+                 'f4','f4','f4','f4','f4',
+                 'f4','f4','f4','f4','f4',
+                 'f4','f4','f4','f4','f4',
+                 'f4','f4','f4','f4','f4',
+		         'f4','f4','f4','f4','f4','f4','i2'))
 
 cv_t = fits.open('tableCV.fits', memmap = True)
 cv_names = Table(cv_t[1].data)['Name']
@@ -63,40 +63,44 @@ def plot_spectra(w, f):
     wb, fb = wb[ok] / nb[ok], fb[ok] / nb[ok] 
     fhi = 1.1 * np.percentile(fb, 99.9)
 	
-    fig=plt.figure(figsize = (12, 6))
-    plt.plot(wb, fb, 'b')
-    title = 'Lines of %s' %(spec[spec.rfind('/') + 1:])
-    plt.title(title.replace('_', '-'))
-    plt.xlabel('Wavelength [\AA]')
-    plt.show()
+    rand = random.randint(0, 2)
+    if rand == 0:
+    #fig=plt.figure(figsize = (12, 6))
+        plt.plot(wb, fb, 'b', color = 'purple')
+    elif rand == 1:
+        plt.plot(wb, fb, 'b', color = 'red')
+    elif rand == 2:
+        plt.plot(wb, fb, 'b', color = 'olive')
+    #title = 'Lines of %s' %(spec[spec.rfind('/') + 1:])
+    #plt.title(title.replace('_', '-'))
+    #plt.xlabel('Wavelength [\AA]')
+    
 
 def flux_var(wavelength):
     _index = np.nonzero(w > wavelength)[0][0]
     range_1 = range(_index - 25, _index + 25)
     range_2 = range(_index - 5, _index + 5)
-    left_range = range(_index - 75, _index - 25)
-    right_range = range(_index + 25, _index + 75)
+    left_range = range(_index - 35, _index - 25)
+    right_range = range(_index + 25, _index + 35)
     
-    t_flux1 = sum(wd[range_1] * f[range_1])
-    t_flux2 = sum(wd[range_2] * f[range_2])
-    t_fluxL = sum(wd[left_range] * f[left_range])
-    t_fluxR = sum(wd[right_range] * f[right_range])
     if 0 in iv[range_1]:
         t_var = 0
     else:
         t_var = sum((1/iv[range_1]) * (wd[range_1] ** 2))
+    t_flux1 = sum(wd[range_1] * f[range_1])
+    t_flux2 = sum(wd[range_2] * f[range_2])
+    t_fluxL = sum(wd[left_range] * f[left_range])
+    t_fluxR = sum(wd[right_range] * f[right_range])
     
     return t_flux1, t_flux2, t_var, t_fluxL, t_fluxR, _index        
 
-
-w_total, f_total, ix = [0]*len(sdss), [0]*len(sdss), 0
+count = 0
 for spec in sdss:
+    count += 1
     hdul = fits.open(spec)
     if (all(hdul[1].data['flux']) == 0):
         continue
-	
-    #print(hdul[3].data)
-
+        
     table = hdul[1].data
     iv = table['ivar']
     ok = iv > 0.
@@ -105,41 +109,32 @@ for spec in sdss:
     f  = table['flux'][ok]
     wd = table['wdisp'][ok]
     
-    #w_total[ix] = w
-    #f_total[ix] = f
-    #ix += 1
-    
     if spec[spec.find('-') + 1:spec.find('.')] in cv_names:
         cls = 1
     else:
         cls = 0
-    h_a = flux_var(6561)
-    h_b = flux_var(4862)
-    x1 = flux_var(5760)
-    x2 = flux_var(6900)
-    x3 = flux_var(4000)
-    he1 = flux_var(5876)
     he2 = flux_var(4686)
+    h_b = flux_var(4862)
+    he1 = flux_var(5876)
+    h_a = flux_var(6561)
     ar3 = flux_var(7135)
     
     t.add_row([spec[spec.find('-') + 1:spec.find('.')],
-			  h_a[3], h_a[0], h_a[1], h_a[4], h_a[2],
-			  h_b[3], h_b[0], h_b[1], h_b[4], h_b[2],
-			  he1[3], he1[0], he1[1], he1[4], he1[2],
-			  he2[3], he2[0], he2[1], he2[4], he2[2],
-			  ar3[3], ar3[0], ar3[1], ar3[4], ar3[2],
-			  x1[3], x1[0], x1[1], x1[4], x1[2],
+              h_a[3], h_a[0], h_a[1], h_a[4], h_a[2],
+              h_b[3], h_b[0], h_b[1], h_b[4], h_b[2],
+              he1[3], he1[0], he1[1], he1[4], he1[2],
+              he2[3], he2[0], he2[1], he2[4], he2[2],
+              ar3[3], ar3[0], ar3[1], ar3[4], ar3[2],
+              hdul[2].data['SPEC2_I'] - hdul[2].data['SPEC2_G'], 
+              hdul[2].data['SPEC2_G'] - hdul[2].data['SPEC2_R'],
 			  cls])
 			  
-    '''plot_spectra(w,f)
-    for e in (h_a, h_b, s_2, he1, he2):
+    '''plot_spectra(w,f)'''
+    '''for e in (h_a, h_b, s_2, he1, he2):
         plot_spectra(w[e[4] - 50:e[4] + 50],
                      f[e[4] - 50:e[4] + 50])'''
-    
-#w_total = sorted([sum(x)/np.count_nonzero(x) for x in longest(*w_total, fillvalue = 0)])
-#f_total = [sum(x)/np.count_nonzero(x) for x in longest(*f_total, fillvalue = 0)]
-#plot_spectra(w_total,f_total)
-
+	
+#plt.show()
 #print(t)
 t.write("table.fits", "w")
-print("%.3fs" % (time.time()-timp))
+print("%.3fs" % (time.time() - start))
